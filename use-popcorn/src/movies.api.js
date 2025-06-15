3500f7f28f3114905fab3fd744caecf2321559bd
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-15 09:56:55 +0300

    feat(use-popcorn): add movies.api class

---
+++ use-popcorn/src/movies.api.js
@@ -0,0 +1,51 @@
+class MoviesApi {
+  #apiKey;
+  #baseUrl;
+
+  constructor(apiKey) {
+    this.#apiKey = apiKey;
+    this.#baseUrl = `http://www.omdbapi.com/?apikey=${apiKey}`;
+  }
+
+  async getMovies({ searchQuery }) {
+    const res = await fetch(
+      `${this.#baseUrl}&s=${searchQuery ? searchQuery : "Interstellar"}`
+    );
+
+    if (!res.ok) throw new Error("Something went wrong with fetching movies");
+
+    const data = await res.json();
+
+    if (data?.Response === "False") {
+      throw new Error("Can't find any movies!");
+    }
+
+    const movies = data.Search;
+
+    return movies.map((movie) => this.#movieMapping(movie));
+  }
+
+  async getMovieById(id) {
+    const res = await fetch(`${this.#baseUrl}&i=${id}&plot=full`);
+
+    if (!res.ok) throw new Error("Something went wrong with fetching movie");
+
+    const data = await res.json();
+
+    return this.#movieMapping(data);
+  }
+
+  #movieMapping(movie) {
+    return {
+      imdbID: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      type: movie.Type,
+      poster: movie.Poster,
+    };
+  }
+}
+
+const omdbApikey = process.env?.REACT_APP_OMDB_API_KEY;
+const moviesApi = new MoviesApi(omdbApikey);
+export default moviesApi;
